78ad48cd7e34be2d52a1dbdd2404a61f2261fe92
teamq20 <70352826+teamq20@users.noreply.github.com>
2020-09-04 14:16:31 -0400

    Multiplayer added to game
    Added Multiplayer feature to game
    Created function to place player on board and to check if the space is filled
    Moved game board to its own function to be called when needed

---
+++ Tic-Tac-Toe Game/tic_tac_toe_game/tic_tac_toe_game/Header.h
@@ -0,0 +1,200 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+using namespace std;
+
+//FUNCTION TO DISPLAY THE GAME BOARD
+void gameBoard(string board[][3]) {
+
+	const int ROWS = 3, COLS = 3;		// Set the number of rows and columns
+
+		//TIC TAC TOE BOARD
+	for (int r = 0; r < ROWS; r++) {							//For loop to create the rows of the board (3 rows)
+
+		for (int c = 0; c < COLS; c++) {						//For loop to create the columns for each row (3 columns per row)
+			cout << setw(3) << " " << board[r][c] << " ";		//Out put the numbers from the board array onto the location specified
+
+			if (c != 2) {
+				cout << "|";									//Divide the columns with a "|"
+			}
+		}
+
+		if (r != 2) {
+			cout << endl << setw(3) << "_________________________" << endl;			//Divide the rows with an underscore
+		}
+	}
+
+	
+}
+
+void position(string board[][3], int pos, string player) {
+
+
+	switch (pos) {						//switch statement to determine which tile on the board to fill in depending on pos parameter
+	case 1:
+		if (board[0][0] == "X" || board[0][0] == "O") {				//if statement to make sure the spot isn't already filled
+																	//Spot is filled
+			cout << "This spot is already filled. Please choose an available spot" << endl;
+			cin >> pos;
+
+			position(board, pos, player);		//Call the position function again with the new choice
+		}
+		else {
+			//Spot is available
+			if (player == "X") {			//If player X chose the spot, fill in 'X'
+				board[0][0] = "[X]";
+			}
+			else if (player == "O") {		//If player O chose the spot, fill in 'O'
+				board[0][0] = "[O]";
+			}
+		}
+		break;
+	case 2:
+		if (board[0][1] == "X" || board[0][1] == "O") {				//if statement to make sure the spot isn't already filled
+																	//Spot is filled
+			cout << "This spot is already filled. Please choose an available spot" << endl;
+			cin >> pos;
+
+			position(board, pos, player);		//Call the position function again with the new choice
+		}
+		else {
+			//Spot is available
+			if (player == "X") {			//If player X chose the spot, fill in 'X'
+				board[0][1] = "[X]";
+			}
+			else if (player == "O") {		//If player O chose the spot, fill in 'O'
+				board[0][1] = "[O]";
+			}
+		}
+		break;
+	case 3:
+		if (board[0][2] == "X" || board[0][2] == "O") {				//if statement to make sure the spot isn't already filled
+																	//Spot is filled
+			cout << "This spot is already filled. Please choose an available spot" << endl;
+			cin >> pos;
+
+			position(board, pos, player);		//Call the position function again with the new choice
+		}
+		else {
+			//Spot is available
+			if (player == "X") {			//If player X chose the spot, fill in 'X'
+				board[0][2] = "[X]";
+			}
+			else if (player == "O") {		//If player O chose the spot, fill in 'O'
+				board[0][2] = "[O]";
+			}
+		}
+		break;
+	case 4:
+		if (board[1][0] == "X" || board[1][0] == "O") {				//if statement to make sure the spot isn't already filled
+																	//Spot is filled
+			cout << "This spot is already filled. Please choose an available spot" << endl;
+			cin >> pos;
+
+			position(board, pos, player);		//Call the position function again with the new choice
+		}
+		else {
+			//Spot is available
+			if (player == "X") {			//If player X chose the spot, fill in 'X'
+				board[1][0] = "[X]";
+			}
+			else if (player == "O") {		//If player O chose the spot, fill in 'O'
+				board[1][0] = "[O]";
+			}
+		}
+		break;
+	case 5:
+		if (board[1][1] == "X" || board[1][1] == "O") {				//if statement to make sure the spot isn't already filled
+																	//Spot is filled
+			cout << "This spot is already filled. Please choose an available spot" << endl;
+			cin >> pos;
+
+			position(board, pos, player);		//Call the position function again with the new choice
+		}
+		else {
+			//Spot is available
+			if (player == "X") {			//If player X chose the spot, fill in 'X'
+				board[1][1] = "[X]";
+			}
+			else if (player == "O") {		//If player O chose the spot, fill in 'O'
+				board[1][1] = "[O]";
+			}
+		}
+		break;
+	case 6:
+		if (board[1][2] == "X" || board[1][2] == "O") {				//if statement to make sure the spot isn't already filled
+																	//Spot is filled
+			cout << "This spot is already filled. Please choose an available spot" << endl;
+			cin >> pos;
+
+			position(board, pos, player);		//Call the position function again with the new choice
+		}
+		else {
+			//Spot is available
+			if (player == "X") {			//If player X chose the spot, fill in 'X'
+				board[1][2] = "[X]";
+			}
+			else if (player == "O") {		//If player O chose the spot, fill in 'O'
+				board[1][2] = "[O]";
+			}
+		}
+		break;
+	case 7:
+		if (board[2][0] == "X" || board[2][0] == "O") {				//if statement to make sure the spot isn't already filled
+																	//Spot is filled
+			cout << "This spot is already filled. Please choose an available spot" << endl;
+			cin >> pos;
+
+			position(board, pos, player);		//Call the position function again with the new choice
+		}
+		else {
+			//Spot is available
+			if (player == "X") {			//If player X chose the spot, fill in 'X'
+				board[2][0] = "[X]";
+			}
+			else if (player == "O") {		//If player O chose the spot, fill in 'O'
+				board[2][0] = "[O]";
+			}
+		}
+		break;
+	case 8:
+		if (board[2][1] == "X" || board[2][1] == "O") {				//if statement to make sure the spot isn't already filled
+																	//Spot is filled
+			cout << "This spot is already filled. Please choose an available spot" << endl;
+			cin >> pos;
+
+			position(board, pos, player);		//Call the position function again with the new choice
+		}
+		else {
+			//Spot is available
+			if (player == "X") {			//If player X chose the spot, fill in 'X'
+				board[2][1] = "[X]";
+			}
+			else if (player == "O") {		//If player O chose the spot, fill in 'O'
+				board[2][1] = "[O]";
+			}
+		}
+		break;
+	case 9:
+		if (board[2][2] == "X" || board[2][2] == "O") {				//if statement to make sure the spot isn't already filled
+																	//Spot is filled
+			cout << "This spot is already filled. Please choose an available spot" << endl;
+			cin >> pos;
+
+			position(board, pos, player);		//Call the position function again with the new choice
+		}
+		else {
+			//Spot is available
+			if (player == "X") {			//If player X chose the spot, fill in 'X'
+				board[2][2] = "[X]";
+			}
+			else if (player == "O") {		//If player O chose the spot, fill in 'O'
+				board[2][2] = "[X]";
+			}
+		}
+		break;
+	}
+
+}
---
+++ Tic-Tac-Toe Game/tic_tac_toe_game/tic_tac_toe_game/Main.cpp
@@ -0,0 +1,111 @@
+#include "Header.h";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string player[2] = { "X", "O" };			//array that holds player letter
+
+	bool p1Turn;								//Boolean variables used to switch between player turns
+	bool p2Turn;								//
+	bool gameOver = false;	//determines if the game is over
+
+	const int ROWS = 3, COLS = 3;		// Set the number of rows and columns
+	string board[ROWS][COLS] = { "(1)", "(2)", "(3)",			//2 dimensional array to create tic tac toe board
+		"(4)", "(5)", "(6)",
+		"(7)", "(8)", "(9)" };
+
+	int menuChoice;						//int to hold the player's input choice in the main menu
+
+										//MAIN MENU
+	cout << "MAIN MENU" << endl;
+	cout << "Tic Tac Toe" << endl;
+	cout << "Rules:" << endl;
+	cout << "-Player 1 is X and Player 2 is O" << endl;
+	cout << "-The game will prompt which player's turn it is. The player will choose an available number from the board" << endl;
+	cout << "-The game ends when a player gets three in a row or when the board is completely filled" << endl;
+	cout << "-The player who gets three in a row wins" << endl << endl;
+
+	cout << "1 - Single Player" << endl;		//These are the player's options
+	cout << "2 - Multiplayer" << endl;			//
+	cout << "3 - Quit" << endl << endl;			//
+
+	cout << "(Enter 1 to play single player, 2 for multiplayer, or 3 to quit the program)" << endl;
+	cin >> menuChoice;
+
+	//Input Validation
+	while (menuChoice < 1 || menuChoice > 3) {			//while loop to make sure the player inputs a valid digit
+		cout << "This is an invalid input!" << endl;
+		cout << "Enter 1 to play single player, 2 for multiplayer, or 3 to quit the program" << endl;
+		cin >> menuChoice;
+	}
+
+	cout << endl;
+
+	//GAME
+	if (menuChoice == 1) {									//If player chooses 1, start SINGLE PLAYER game
+		cout << "SINGLE PLAYER" << endl;
+
+		gameBoard(board);			//Call gameBoard function to display tic tac toe board
+
+	}
+	else if (menuChoice == 2) {								//If player chooses 2, start MULTIPLAYER game
+		int p1Choice, p2Choice;
+		cout << "MULTIPLAYER" << endl;
+
+		p1Turn = true;				//Set p1turn to true to begin the game
+
+		while (!gameOver) {			//while loop to keep the game in session
+
+			if (p1Turn == true) {		//if/else statement to switch between players during the game
+
+				gameBoard(board);			//Call gameBoard function to display tic tac toe board
+				cout << endl;
+
+				cout << "Player " << player[0] << " choose an available spot on the board" << endl;	
+				cin >> p1Choice;			//Player 1 inputs desired spot on board
+
+				while (p1Choice < 1 || p1Choice > 9) {		//Input validation (make sure player chooses a spot on the board
+					cout << "This is not a valid spot. Please input a number from 1-9.";
+					cin >> p1Choice;
+				}
+
+				position(board, p1Choice, player[0]);		//Call the position function to place the player on the desired position on the board
+
+				p1Turn = false;
+				p2Turn = true;
+
+			}
+			else if (p2Turn == true) {
+
+				gameBoard(board);			//Call gameBoard function to display tic tac toe board
+				cout << endl;
+
+				cout << "Player " << player[1] << " choose an available spot on the board" << endl;
+				cin >> p2Choice;			//Player 1 inputs desired spot on board
+
+				while (p2Choice < 1 || p2Choice > 9) {		//Input validation (make sure player chooses a spot on the board
+					cout << "This is not a valid spot. Please input a number from 1-9.";
+					cin >> p2Choice;
+				}
+
+				position(board, p2Choice, player[1]);		//Call the position function to place the player on the desired position on the board
+
+				p1Turn = true;
+				p2Turn = false;
+
+			}
+		}
+
+		position(board, p1Choice, player[0]);
+	}
+	else if (menuChoice == 3) {								//If player chooses 3, exit the program
+		cout << "QUIT" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
