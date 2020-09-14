201085775b39ada6f61269e2999717d20c2d69b1
teamq20 <teamq20s@gmail.com>
2020-09-14 17:15:03 -0400

    out with the old, in w/ the new project
    updated the repo w/ the version of the game that addressed recusive functions (their removal) and also improved upon the UI

---
+++ Tic-Tac-Toe Game/Tic_Tac_Toe_Recursion_Eliminated/tic tac toe/tic tac toe/Header.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+using namespace std;
+
+//A structure that assigns (x,y) positions	
+//on the game board to a numbered position	
+//from 1 to 9								
+static struct boardPositions {
+	int pos;
+	int boardX;
+	int boardY;
+};
+
+//function prototypes
+int inputValidate(int, int, int);
+void singlePlayer(string[], bool, bool, boardPositions position[]);
+void multiPlayer(string[], bool, bool, boardPositions position[]);
+void displayMenu(boardPositions position[]);
+void gameBoard(string[][3]);
+void fillPosition(string[][3], int, string, boardPositions position[]);
+bool positionAvailable(boardPositions position[], int, string[], string[][3]);
+bool checkWinner(string[][3], string);
+void endScreen(boardPositions position[]);
