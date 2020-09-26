0ac11f15eb13dd8e3b50cf3d5b1b91837114dae2
Shmeatlord-2D <57604526+Shmeatlord-2D@users.noreply.github.com>
2020-09-26 18:40:38 -0400

    started the story

--- Text-Based Game/Team_20_TextBasedGame/Team_20_TextBasedGame/Source.cpp
+++ Text-Based Game/Team_20_TextBasedGame/Team_20_TextBasedGame/Source.cpp
@@ -6,4 +6,11 @@ using namespace std;
 int main() {
 
 	mainMenu();
+	string Starting_Items[8] = { "Pickaxe","Drill","Bucket","Flask","Knife","Gun","Flashlight","Lantern" };
+
+	cout << "While looking through the wreckage of the Demeter, you found out that the exploration dispensery was damaged, but still operational." << endl
+		<< "When you selected the tools needed, the dispensery can only operate to an extent that it can only dispense one item of each catagory." << endl
+		<< "for exploration, the catagories are:" << endl << "Excavation" << endl << "Liquid contaiment" << "Weaponry" << "Illumination" << endl << endl
+		<< "In the feild of Excavation, would you prefer:" << endl << "Pickaxe" << endl << "or" << endl << "Drill" << endl;
+	
 }
