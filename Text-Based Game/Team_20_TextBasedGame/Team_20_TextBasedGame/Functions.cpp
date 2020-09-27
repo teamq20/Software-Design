f2e4f327f84b71fcabfc5c6deeb873daa236ab01
Shmeatlord-2D <57604526+Shmeatlord-2D@users.noreply.github.com>
2020-09-26 20:48:33 -0400

    fixed

--- Text-Based Game/Team_20_TextBasedGame/Team_20_TextBasedGame/Functions.cpp
+++ Text-Based Game/Team_20_TextBasedGame/Team_20_TextBasedGame/Functions.cpp
@@ -1,10 +1,17 @@
 #include "Game_Classes.h"
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
+<<<<<<< Updated upstream
 void mainMenu() {
 	string menuChoice;
+=======
+void mainMenu() 
+{
+	int menuChoice;
+>>>>>>> Stashed changes
 
 	cout << "ESCAPE FROM MINERVA" << endl;
 	cout << "(1) Single Player" << endl;
@@ -16,6 +23,7 @@ void mainMenu() {
 	int input = convertToInt(menuChoice);
 	inputValidation(input);
 
+<<<<<<< Updated upstream
 	if (input == 1) {
 		singlePlayer();
 	}
@@ -23,24 +31,49 @@ void mainMenu() {
 		multiPlayer();
 	}
 	else if (input == 0) {
+=======
+	if (menuChoice == 1) 
+	{
+		singlePlayer();
+	}
+	else if (menuChoice == 2) 
+	{
+		multiPlayer();
+	}
+	else if (menuChoice == 0)
+	{
+>>>>>>> Stashed changes
 		cout << "Exit" << endl;
+		_Exit(10);
 	}
 }
 
 void singlePlayer() {
 	cout << "SINGLE PLAYER MODE" << endl;
 
-	Story story;
+	Story *story = new Story();
+	story->setIntro();
 
+<<<<<<< Updated upstream
 	string intro = story.getIntro(1);
+=======
+	string intro = story->getIntro(1);
+	cout << intro << endl <<endl <<endl;
+>>>>>>> Stashed changes
 }
 
 void multiPlayer() {
 	cout << "MULTIPLAYER MODE" << endl;
 
-	Story story;
+	Story *story2 = new Story();
+	story2->setIntro();
 
+<<<<<<< Updated upstream
 	string intro = story.getIntro(1);
+=======
+	string intro = story2->getIntro(2);
+	cout << intro << endl <<endl <<endl;
+>>>>>>> Stashed changes
 }
 
 int convertToInt(string input) {
@@ -48,8 +81,15 @@ int convertToInt(string input) {
 	return choice;
 }
 
+<<<<<<< Updated upstream
 void inputValidation(int input) {
 	while (input < 0 || input > 2) {
+=======
+void inputValidation(int input) 
+{
+	while (input < 0 || input > 2) 
+	{
+>>>>>>> Stashed changes
 		string menuChoice;
 
 		cout << "This is an invalid input" << endl;
@@ -59,3 +99,4 @@ void inputValidation(int input) {
 		input = convertToInt(menuChoice);
 	}
 }
+
