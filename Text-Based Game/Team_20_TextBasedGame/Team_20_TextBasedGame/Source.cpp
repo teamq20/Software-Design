539f5d7fc8ead236aa6ca29389df8755efc78fa7
teamq20 <70352826+teamq20@users.noreply.github.com>
2020-10-02 23:23:25 -0400

    Location Selection
    Created a location selection function where the user selects where on Minerva they would like to go.
    Options Menu created and health bar has also been created.

--- Text-Based Game/Team_20_TextBasedGame/Team_20_TextBasedGame/Source.cpp
+++ Text-Based Game/Team_20_TextBasedGame/Team_20_TextBasedGame/Source.cpp
@@ -1,17 +1,27 @@
-#include "Game_Classes.h"
+#include "Header.h"
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
+	Minerva north, south, east, west;
 
-	mainMenu();
+	north.setSetting("Volcano");
+	north.setItem("Uranium");
+
+	south.setSetting("Liquid Streams");
+	south.setItem("Hydrazine");
+
+	east.setSetting("Cave");
+	east.setItem("Copper");
+
+	west.setSetting("Clusters of Rock");
+	west.setItem("Fluorine");
+
 	string Starting_Items[8] = { "Pickaxe","Drill","Bucket","Flask","Knife","Gun","Flashlight","Lantern" };
 
-	cout << "While looking through the wreckage of the Demeter, you found out that the exploration dispensery was damaged, but still operational." << endl
-		<< "When you selected the tools needed, the dispensery can only operate to an extent that it can only dispense one item of each catagory." << endl
-		<< "\nFor exploration, the categories are:\n" << "Excavation\n" << "Liquid containment\n" << "Weaponry\n" << "Illumination\n" << endl
-		<< "In the field of Excavation, would you prefer:\n" << "Pickaxe\n" << "or\n" << "Drill" << endl;
-	
+	mainMenu();
+
 	system("pause");
+	return 0;
 }
