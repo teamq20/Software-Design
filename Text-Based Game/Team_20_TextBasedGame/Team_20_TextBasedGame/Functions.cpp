1ca4825f0286620313f0c55e72f1e94d3b3a91c3
Christina <cls18@student.fdu.edu>
2020-10-08 22:34:39 -0400

    Revert "Revert "Merge branch 'master' into Nolasco_Location_Choice""
    This reverts commit 5133b8a9d70e8980983b45195a62b7b7a39c88e7.

--- Text-Based Game/Team_20_TextBasedGame/Team_20_TextBasedGame/Functions.cpp
+++ Text-Based Game/Team_20_TextBasedGame/Team_20_TextBasedGame/Functions.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <cctype>
 using namespace std;
 
 void mainMenu() {
@@ -139,7 +140,7 @@ void gameIntro(int playerMode) {
 	}
 
 	cout << "While looking through the wreckage of the Demeter, you found out that the exploration dispensery was damaged, but still operational." <<
-		"\nFour systems need repair:" <<
+		"\n\nFour systems need repair:" <<
 		"\n-Reactor Core\n-Navigation System\n-Left Thruster\n-Oxidizer";
 
 	gameRules();
@@ -147,7 +148,7 @@ void gameIntro(int playerMode) {
 
 void locationSelection() {
 	string choice;
-	cout << "What region of Minerva do you want to explore?" << endl;
+	cout << "\nWhat region of Minerva do you want to explore?" << endl;
 	cout << "(1) North: Minerva Volcanos" << endl;
 	cout << "(2) East: Caves" << endl;
 	cout << "(3) South: Liquid Streams" << endl;
@@ -158,16 +159,16 @@ void locationSelection() {
 	inputValidation(input, 4);
 
 	if (input == 1) {
-		cout << "LOCATION: Minerva Volcanos" << endl;
+		cout << "\nLOCATION: Minerva Volcanos" << endl;
 	}
 	else if (input == 2) {
-		cout << "LOCATION: Caves" << endl;
+		cout << "\nLOCATION: Caves" << endl;
 	}
 	else if (input == 3) {
-		cout << "LOCATION: Liquid Streams" << endl;
+		cout << "\nLOCATION: Liquid Streams" << endl;
 	}
 	else if (input == 4) {
-		cout << "LOCATION: Clusters of Rocks" << endl;
+		cout << "\nLOCATION: Clusters of Rocks" << endl;
 	}
 }
 
@@ -177,13 +178,13 @@ void singlePlayer() {
 	gameIntro(1);
 	static int health = 30;
 	bool ship = "";
-	
-
+  
 	Beginning_stage();
+
 
 	do {
 		string choice;
-		cout << "LOCATION: Demeter" << endl << endl;
+		cout << "\nLOCATION: Demeter" << endl << endl;
 		cout << "What is your next move?" << endl;
 		cout << "(1) Explore Minerva" << endl;
 		cout << "(2) Repair Ship" << endl;
@@ -219,9 +220,9 @@ void multiPlayer() {
 void options(int playerMode, int health) {
 	string menuChoice;
 
-	cout << "HEALTH: " << health;
+	cout << "[HEALTH: " << health << "]";
 
-	cout << "OPTIONS MENU" << endl;
+	cout << "\nOPTIONS MENU" << endl;
 	cout << "(1) Go to main menu" << endl;
 	cout << "(2) Reset Game" << endl;
 	cout << "(0) Quit" << endl;
@@ -248,7 +249,7 @@ void options(int playerMode, int health) {
 
 void gameRules() {
 
-	cout << "Objective: Repair the ship so you can get off the planet. You will be given two or more options at each stage./nYour choices impact your survival." << endl;
+	cout << "\n\nObjective: Repair the ship so you can get off the planet. You will be given two or more options at each stage./nYour choices impact your survival." << endl;
 	cout << "Collect 4 specific items to repair your ship" << endl;
 	cout << "Input '0' at any time to open the options menu. Here you will be able to see your inventory, repair status, and health." << endl;
 }
@@ -259,7 +260,7 @@ int convertToInt(string input) {
 	while (stringCounter != 0) {
 		stringCounter = 0;
 		for (int i = 0; i < input.length(); i++) {
-			if (isalpha(input[i])) {
+			if (isdigit(input[i]) == false) {
 				stringCounter++;
 			}
 		}
